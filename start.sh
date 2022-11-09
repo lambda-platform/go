d00270f32529588d4833392a99aefb08e8c44bc6
munkh-altai <munkaltai@gmail.com>
2022-11-09 08:39:14 +0800

    start.sh updated

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
-sudo rm -R ./lambda
+rm -R ./lambda
 echo "INIT START"
-sudo mkdir ./lambda
-sudo chmod -R 777 ./lambda
+mkdir ./lambda
+chmod -R 777 ./lambda
 go run ./bootstrap/init/init.go
-sudo chmod -R 755 ./lambda
+chmod -R 755 ./lambda
 echo "Ready"
 go run main.go
