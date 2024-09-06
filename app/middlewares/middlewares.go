30d8c75ec406b2b24b63d98c341c2b71408b2349
Bilguun <4bilguun@gmail.com>
2024-09-06 12:34:42 +0800

    fixes cors warning

--- app/middlewares/middlewares.go
+++ app/middlewares/middlewares.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/helmet"
 	"github.com/gofiber/fiber/v2/middleware/recover"
+	"strings"
 )
 
 func Set(app *fiber.App) {
@@ -13,14 +14,25 @@ func Set(app *fiber.App) {
 		| Useful MIDDLEWARES
 		|----------------------------------------------
 	*/
-	app.Use(helmet.New())
+	app.Use(helmet.New(helmet.Config{
+		CrossOriginEmbedderPolicy: "unsafe-none",
+		CrossOriginOpenerPolicy:   "unsafe-none",
+		CrossOriginResourcePolicy: "unsafe-none",
+	}))
 	app.Use(recover.New())
 	app.Use(cors.New(cors.Config{
 		AllowCredentials: true,
-		AllowOriginsFunc: func(origin string) bool { return true },
-		AllowOrigins:     "*",
 		AllowHeaders:     "Origin, Content-Type, Accept, X-Requested-With, x-csrf-token, Accept-Language, Content-Length, Authorization, Accept-Encoding, Connection",
-		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH",
+		AllowOriginsFunc: nil,
+		AllowOrigins:     "http://localhost:3000, http://127.0.0.1:3000, http://localhost:8080, http://127.0.0.1:8080",
+		AllowMethods: strings.Join([]string{
+			fiber.MethodGet,
+			fiber.MethodPost,
+			fiber.MethodHead,
+			fiber.MethodPut,
+			fiber.MethodDelete,
+			fiber.MethodPatch,
+		}, ","),
 	}))
 
 }
